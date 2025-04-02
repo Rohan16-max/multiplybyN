86a6c46a739c4551401abd5ebbee9ce9ed53a083
Rohan16-max <rohan.kr.mitra@gmail.com>
2025-04-02 23:41:29 +0600

    MultiplyByN.py

--- MultiplyByN.py
+++ MultiplyByN.py
@@ -1,11 +1,11 @@
-# Function 1: Multiply using a loop (Iteration-based multiplication)
+# Function 1: Multiply using a loop 
 def multiply_using_iteration(N, M):
     result = 0
     for _ in range(M):
         result += N
     return result
 
-# Function 2: Multiply using N iterations (Repeated addition)
+# Function 2: Multiply using N iterations 
 def multiply_using_N_iterations(N, M):
     result = 0
     for _ in range(N):
@@ -19,11 +19,11 @@ def main():
     
     # Using iteration-based multiplication
     result_1 = multiply_using_iteration(N, M)
-    print(f"Result using iteration-based multiplication: {result_1}")
+    print(f"Result using iteration-based multiplication:- {result_1}")
     
     # Using N iterations 
     result_2 = multiply_using_N_iterations(N, M)
-    print(f"Result using N iterations (repeated addition): {result_2}")
+    print(f"Result using N iterations :- {result_2}")
 
 # Run the program
 if __name__ == "__main__":
