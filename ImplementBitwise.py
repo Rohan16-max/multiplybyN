2f22549a2972c686bbefefe4304e710c660ee9cc
Rohan16-max <rohan.kr.mitra@gmail.com>
2025-04-22 16:43:41 +0600

    Add files via upload

---
+++ ImplementBitwise.py
@@ -0,0 +1,17 @@
+x = 12
+z = 15
+
+print(~x)
+print(~z)
+
+print(x & z)
+
+print(x|z)
+
+print(x^z)
+
+print(x>>1)
+print(z>>1)
+
+print(x<<1)
+print(z<<1)
---
+++ OddEvenBitwise.py
@@ -0,0 +1,6 @@
+x = int(input("Enter a number :-"))
+
+if x^1==x+1:
+    print("Its a Even number.")
+else:
+    print(" Its a Odd number .")
